b18958620e3a1abe5a158fe5c4d661c3ff256401
Chris Paul <chrispaul3625@gmail.com>
2016-01-13 16:11:34 -0700

    Updates to style.css

--- public_html/aboutme.php
+++ public_html/aboutme.php
@@ -2,26 +2,34 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
+
+		<!---Normailze stylesheet--->
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+		<!-----Custom stylesheet --->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<title>About Me</title>
 	</head>
 		<body>
 			<header>
 				<h1>Welcome to my webpage! Below is a little bit of information about me!</h1>
-					<img src="images/picture.jpg" alt="Pic of me">
-				<h2>A Little Bit About Me </h2>
 
-				<p> Hello my name is Chris Paul, I am 27 years old.
+				<p><img src="images/picture.jpg" alt="Pic of me"></p>
+
+				<h2>A Little Bit About Me </h2>
+<ul>
+				<li> Hello my name is Chris Paul, I am 27 years old.
 					I grew up on a small island in the Pacific Northwest called
 					whidbey island in a small town called
 					Anacortes Washington. My mother (Tereza) raised me and my siblings as
 					a single parent, and I have 2 siblings.
 					I am the middle child of the family my brother
-					Eric being the oldest and my sister Ashley being the youngest.</p>
+					Eric being the oldest and my sister Ashley being the youngest.</li>
+	</ul>
+
 				<h3> Interests </h3>
 				<ul>
-					<li> Soccer, this is my favorite hobby/interest </li>
+					<li> Soccer, this is my favorite hobby/interest. </li>
 					<li> I love to be outdoors, camping, hiking, paddle boarding, kayaking etc.</li>
 					<li> Working out at the gym. </li>
 					<li> Hot yoga. </li>
@@ -30,6 +38,6 @@
 			</header>
 	</body>
 	<footer>
-		<p>Thanks for visiting! </p>
+		<p class="closing"> Thanks for visiting! </p>
 	</footer>
 </html>
