b5a96ba5dac840ad027f0bc6a8f30ec53b44e3db
Chris Paul <chrispaul3625@gmail.com>
2016-01-13 12:52:26 -0700

    Uploaded picture and some content

---
+++ public_html/aboutme.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>About Me</title>
+	</head>
+		<body>
+			<header>
+			<h1>Welcome!</h1>
+				<img src="public_html/images/picture.jpg" alt="Pic of me">
+			<h2>A Little Bit About Me </h2>
+			<p> Hello my name is Chris Paul, I am 27 years old. I grew up on a small island in the Pacific Northwest called
+				whidbey island in a small town called Anacortes Washington. My mother (Tereza) raised me and my siblings as
+				a single parent, and I have 2 siblings. I am the middle child of the family my brother
+				Eric being the oldest and my sister Ashley being the youngest.</p>
+			<h3> Interests </h3>
+			<ul>
+				<li> Soccer, this is my favorite hobby/interest </li>
+			</ul>
+
+	</header>
+
+
+	</body>
+</html>
